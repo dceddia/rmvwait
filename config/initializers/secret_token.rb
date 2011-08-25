e757c6c26e97ca0492ed89cbba20c21886380e4a
Dave Ceddia <dceddia@gmail.com>
2011-08-24 23:25:02 -0400

    rails skeleton generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmvwait::Application.config.secret_token = '539d54f94d4891f389f0ac760058c11a779f1a4db0d2913a644f3d4e4ab56c0c34ec4669ff3aa7b9d512827026db4182c3cd5a349a10093f967ba3be524bf58b'
