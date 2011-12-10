2e3a226edcb79471f2adcfc47d64ef5b1bdd33b5
Dave Ceddia <dceddia@gmail.com>
2011-12-10 16:15:44 -0500

    fixed typo in RMVDuration regex

--- lib/rmv_duration.rb
+++ lib/rmv_duration.rb
@@ -2,15 +2,13 @@
 #  Copyright (c) 2011. All rights reserved.
 
 module RMVDuration
-  include ChronicDuration
-  
   # Wayyy faster than ChronicDuration.parse
   # about .08 seconds for 5000 records vs 6.7 seconds
   def parse(str)
-    days = (str =~ /(\d+)\w+?d/) ? $1.to_i : 0
-    hours = (str =~ /(\d+)\w+?h/) ? $1.to_i : 0
-    minutes = (str =~ /(\d+)\w+?m/) ? $1.to_i : 0
-    seconds = (str =~ /(\d+)\w+?s/) ? $1.to_i : 0
+    days = (str =~ /(\d+)\s+?d/) ? $1.to_i : 0
+    hours = (str =~ /(\d+)\s+?h/) ? $1.to_i : 0
+    minutes = (str =~ /(\d+)\s+?m/) ? $1.to_i : 0
+    seconds = (str =~ /(\d+)\s+?s/) ? $1.to_i : 0
     seconds + minutes*60 + hours*3600 + days*86400
   end
 
