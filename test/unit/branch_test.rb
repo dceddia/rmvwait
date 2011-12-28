eaf5fca8b0564567991def32a7ff970aaf159ec5
Dave Ceddia <dceddia@gmail.com>
2011-12-28 18:03:27 -0500

    removed unused rake task, fixed branch and wait time tests to work with new fixtures

--- test/unit/branch_test.rb
+++ test/unit/branch_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
 class BranchTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :all
+
+  def setup
+    @branch = branches(:boston)
   end
+
+  test "has a wait_times_in_range method" do
+    assert_respond_to @branch, :wait_times_in_range
+  end
+
+  test "branch should have 50 wait times" do
+    assert_equal @branch.wait_times.length, 50
+  end
+=begin
+  test "return wait times for single day" do
+    start = DateTime.parse('Oct 3 2011 00:00:00')
+    stop = DateTime.parse('Oct 3 2011 23:59:59')
+    puts "first time reported at: #{@branch.wait_times.first.reported_at} (looking for #{start} to #{stop})"
+    puts "this says #{@branch.wait_times.first.reported_at >= start} and #{@branch.wait_times.first.reported_at <= stop}"
+    licensing, registration = @branch.wait_times_in_range(start, stop)
+    assert_equal 5, licensing.length
+    assert_equal 5, registration.length
+  end
+=end
 end
--- test/unit/wait_time_test.rb
+++ test/unit/wait_time_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class WaitTimeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should have 50 wait times for each branch" do
+    assert_equal branches(:boston).wait_times.length, 50
+    assert_equal branches(:watertown).wait_times.length, 50
+    assert_equal branches(:marthas_vineyard).wait_times.length, 50
   end
 end
