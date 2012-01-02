25a760a06ea9a80bcb307fea473f2cba4a3b21dd
Dave Ceddia <dceddia@gmail.com>
2012-01-02 14:23:47 -0500

    counting the number of '|' is slightly faster than splitting on it

--- sandbox/split_benchmark.rb
+++ sandbox/split_benchmark.rb
@@ -7,9 +7,11 @@ lines = File.readlines("split_data")
 split1_result = []
 split2_result = []
 regex_result = []
+count_result = []
 
 Benchmark.bm(5) do |bm|
   bm.report { lines.each { |l| split1_result << l.split('|') } }
   bm.report { lines.each { |l| split2_result << l.split(/|/) } }
   bm.report { lines.each { |l| l =~ /(.*)|(.*)|(.*)|(.*)|(.*)/; regex_result << [$1, $2, $3, $4, $5] } }
+  bm.report { lines.each { |l| count_result << l.count('|') } }
 end
