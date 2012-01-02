a6a26c04a0a0e0a06614e0db8c0f67674639159a
Dave Ceddia <dceddia@gmail.com>
2012-01-02 17:52:05 -0500

    spork will reload models now

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,8 @@ end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
-  
+  ActiveSupport::Dependencies.clear
+  ActiveRecord::Base.instantiate_observers
 end
 
 # --- Instructions ---
