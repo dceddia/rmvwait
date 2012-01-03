008c1a31a25210c42cbb9ebe3694f4bc9f285be9
Dave Ceddia <dceddia@gmail.com>
2012-01-02 21:25:26 -0500

    updated wait time loader

--- extras/wait_time_utils.rb
+++ extras/wait_time_utils.rb
@@ -123,4 +123,69 @@ module WaitTimeUtils
       end
     end
   end
+  
+  class WaitTimeLoader
+    def initialize(batch_size = 2)
+      @filters = []
+      @filters << WaitTimeFilters::DiscardMalformedLine.new \
+               << WaitTimeFilters::DiscardBadStatusLine.new      
+      @column_names = [:branch_id, :duration, :reported_at, :kind]
+      @branch_cache = {}
+      @wait_times = []
+      @batch_size = batch_size
+    end
+    
+    def get_branch(branch_name)
+      if not @branch_cache.include?(branch_name)
+        @branch_cache[branch_name] = Branch.where(:human_name => branch_name).first
+      end
+      @branch_cache[branch_name]
+    end
+       
+    def send_batch(force = false)
+      if force or @wait_times.length >= @batch_size
+        WaitTime.import @column_names, @wait_times  
+        @wait_times = []                                                    
+      end
+    end
+    
+    def load_line(line)
+      orig_line = line.dup
+      @filters.each do |f|
+        line = f.modify(line)
+        return false if line.nil?
+      end
+      
+      branch, licensing_duration, registration_duration, reported_at, retrieved_at = parse_line(line)
+
+      @wait_times << [branch.object_id, licensing_duration, reported_at, :licensing]
+      @wait_times << [branch.object_id, registration_duration, reported_at, :registration]
+      send_batch
+      true
+    end
+    
+    def parse_line(line)
+      branch_name, licensing_wait, registration_wait, reported_time, retrieved_at = line.split("|")
+      licensing_duration = RMVDuration.parse(licensing_wait)
+      registration_duration = RMVDuration.parse(registration_wait)
+      reported_time = DateTime.parse(reported_time).utc
+      reported_at = DateTime.parse(retrieved_at).change(:hour => reported_time.hour, 
+                                                        :min => reported_time.min).utc
+      branch = get_branch(branch_name)
+      [branch, licensing_duration, registration_duration, reported_at, retrieved_at]
+    end
+    
+    def load_file(file)
+      lines_total = lines_loaded = 0
+      IO.foreach(file) do |line|
+        lines_loaded += 1 if load_line(line)
+        lines_total += 1
+        if (lines_total % 1000 == 0)
+          puts "Loaded %d / %d" % [lines_loaded, lines_total]
+        end
+      end
+      send_batch(true)
+      [lines_loaded, lines_total]
+    end
+  end
 end
--- lib/tasks/load_wait_times.rake
+++ lib/tasks/load_wait_times.rake
@@ -23,47 +23,8 @@ task :load_wait_times, [:filename] => [:environment] do |t, args|
     exit
   end
   
-  branch_cache = {}
-  branch = nil
-  i = 0
-  wait_times = []
-  IO.foreach(args[:filename]) do |line|
-    next unless valid_line?(line)
-    split_line = line.split('|')
-    next unless split_line.length == 5
-    branch_name, licensing_wait, registration_wait, reported_time, retrieved_at = split_line
-
-    licensing_duration = RMVDuration.parse(licensing_wait)
-    registration_duration = RMVDuration.parse(registration_wait)
-    reported_time = Time.parse(reported_time)
-    reported_at = Time.parse(retrieved_at).change(:hour => reported_time.hour, 
-                                                  :min => reported_time.min)
-
-    if branch_cache.include?(branch_name)
-      branch = branch_cache[branch_name]
-    else
-      branch = Branch.where(:human_name => branch_name).first
-      branch_cache[branch_name] = branch
-    end
-      
-    column_names = [:branch_id, :duration, :reported_at, :kind]
-    wait_times << [branch.object_id, licensing_duration, reported_at, :licensing]
-    wait_times << [branch.object_id, registration_duration, reported_at, :registration]
-=begin
-    wait_times << WaitTime.new(:branch => branch,
-                               :duration => licensing_duration,
-                               :reported_at => reported_at,
-                               :kind => :licensing)
-    wait_times << WaitTime.new(:branch => branch,
-                               :duration => registration_duration,
-                               :reported_at => reported_at,
-                               :kind => :registration)
-=end
-    if (i % 1000) == 0
-      WaitTime.import column_names, wait_times
-      wait_times = []
-      puts "Loaded #{i}"
-    end
-    i += 1
-  end
+  wtl = WaitTimeUtils::WaitTimeLoader.new(2000)
+  loaded, total = wtl.load_file(args[:filename])
+  
+  puts "Loaded #{loaded} of #{total} lines (%.01f)" % [(loaded.to_f / total.to_f) * 100.0]
 end
